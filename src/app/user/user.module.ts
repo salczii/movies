0734e8e8dc2778612ea83fe748e58b6b7742ac83
salczii <kacper.salek32@gmail.com>
2023-05-10 13:05:57 +0200

    add modal component and render it into an app component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthModalComponent
+  ],
+  exports: [
+    AuthModalComponent
+  ],
   imports: [
     CommonModule
   ]
